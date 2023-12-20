6eb960a3ddc34ece3b6b2b4aeed8d5e3d2971606
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-20 23:25:39 +0000

    Feat(page): Home Page & Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
-import { Text, Title, Center, Code } from "@mantine/core";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
+"use client"
 
-export default async function Home() {
+import { Text, Title, Center, Code, Button } from "@mantine/core";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Shell from "./components/Shell/Shell";
+
+export default function Home() {
+  const session = useSession();
 
   return (
     <>
-      <Center>
-        <Title>Mantine, TRPC, Prisma, NextAuth + App Directory Template</Title>
-      </Center>
+        <Center>
+          <Title>Mantine, TRPC, Prisma, NextAuth + App Directory Template</Title>
+        </Center>
 
-      <Center>
-        <Text>🎈 Mantine + T3 Stack Starter Kit</Text>
-      </Center>
+        <Center>
+          <Text>🎈 Mantine + T3 Stack Starter Kit</Text>
+        </Center>
 
-      <Center>
-        <Text>Edit <Code>src/app/page.tsx</Code> to get started</Text>
-      </Center>
+        <Center>
+          <Text>Edit <Code>src/app/page.tsx</Code> to get started</Text>
+        </Center>
+
+        {session.status === "authenticated" ? <Text>Welcome, {session.data.user.name}!</Text> : null}
     </>
   );
 }
