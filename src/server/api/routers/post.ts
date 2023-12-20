aa7408fef585a2f571440ab932866f522a1aef91
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-20 23:27:01 +0000

    Feat(post): New Post

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,28 +3,24 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
-  publicProcedure,
 } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ 
+      title: z.string().min(1),
+      body: z.string().min(1),
+      tags: z.string().min(1),
+    }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          title: input.title,
+          body: input.body,
+          tags: input.tags,
+
           createdBy: { connect: { id: ctx.session.user.id } },
+          createdAt: new Date(),
         },
       });
     }),
@@ -34,9 +30,5 @@ export const postRouter = createTRPCRouter({
       orderBy: { createdAt: "desc" },
       where: { createdBy: { id: ctx.session.user.id } },
     });
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
   }),
 });
