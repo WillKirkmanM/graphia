23bb566423ae908d8b09c32026410dd2ec01897a
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-21 22:03:10 +0000

    Feat(api): Added Tag Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
+import { tagRouter } from "~/server/api/routers/tag";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  tag: tagRouter
 });
 
 // export type definition of API
