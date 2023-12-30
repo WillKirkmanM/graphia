f9869b881269b6bba15eb21d311322b928942874
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-30 18:05:53 +0000

    Feat(user): Update & Find User with Username Implementations

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const userRouter = createTRPCRouter({
+  updateUsername: protectedProcedure
+    .input(z.object({ id: z.string().min(1), username: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.update({
+        where: { id: input.id },
+        data: { username: input.username }
+      });
+    }),
+
+  findByUsername: protectedProcedure
+    .input(z.object({ username: z.string().min(1) }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.user.findUnique({
+        where: { username: input.username }
+      });
+    })
+})
+
