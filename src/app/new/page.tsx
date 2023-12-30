cfa35b92bcfb36349b64f16c69fb2157f780981a
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-30 18:03:23 +0000

    Feat(new): Implementation of TagSelection & PostCard

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { Textarea, Fieldset, Button, ActionIcon, Group, Title, MultiSelect, Text, Container, Flex, Divider } from "@mantine/core";
-import Link from "next/link";
+import { Textarea, Fieldset, Button, ActionIcon, Group, MultiSelect, Combobox, useCombobox,  } from "@mantine/core";
 import { IconSettings } from "@tabler/icons-react";
 import { useState } from "react";
-import Markdown from "markdown-to-jsx";
 import { api } from "~/trpc/react";
 import slugify from "slugify";
+import TagSelection from "../components/Posts/TagSelection";
+import PostCard from "../components/Posts/PostCard";
 
 export default function New() {
   const [title, setTitle] = useState("")
@@ -25,24 +25,13 @@ export default function New() {
           <>
             <Textarea placeholder="Your Post Title" value={title} autosize minRows={1} mb={10} onChange={(e) => { setTitle(e.currentTarget.value); setSlug(slugify(e.currentTarget.value)) }}/>
             <Textarea placeholder="The desired url" value={slug} autosize minRows={1} mb={10} onChange={(e) => setSlug(e.currentTarget.value)}/>
-            <MultiSelect placeholder="Enter up to 5 tags..." data={tagList ? [...tagList].sort((a, b) => b.popularity - a.popularity).map(tag => tag.name) : []} onChange={(e) => setTags(e.join(","))} value={tags ? tags.split(",") : []}  searchable/>
+            {/* <MultiSelect placeholder="Enter up to 5 tags..." data={tagList ? [...tagList].sort((a, b) => b.popularity - a.popularity).map(tag => tag.name) : []} onChange={(e) => setTags(e.join(","))} value={tags ? tags.split(",") : []} maxValues={5} searchable /> */}
+            <TagSelection />
             <Textarea placeholder="Write your post content here..." value={body} autosize minRows={4} mt={10} onChange={(e) => setBody(e.currentTarget.value)}/>
           </>
         ) : (
           <>
-            <Title>{title}</Title>
-            {slug ? <Link href={window.location.host + "/" + slug}><Text c="blue">{window.location.host + "/" + slug}</Text></Link> : null}
-            <Flex wrap="wrap" my="xs">
-            {tags ? tags.split(",").map((tag, index) => (
-              <Container bg="#f1f3f5" size="sm" px={10} mx={5} style={{ borderRadius: '10px', padding: 0, margin: 'auto', display: 'inline-block' }}>
-                <Text key={index}>#{tag}</Text>
-              </Container>
-              )) : null}
-            </Flex>
-
-            <Divider my="md" />
-
-            <Markdown>{body}</Markdown>
+            <PostCard post={{ title, slug, tags, body, createdAt: new Date(), updatedAt: new Date(), createdById: "0", id: 0}}/>
           </>
         )}
 
