e737e2b63bc373f8a37bf133d916fb0cb0c8f73e
WillKirkmanM <WillKirkmanMoller@gmail.com>
2023-12-30 18:01:41 +0000

    Feat(PostList): Integrated PostCard with PostList

--- src/app/components/Posts/PostList.tsx
+++ src/app/components/Posts/PostList.tsx
@@ -1,9 +1,6 @@
 "use client"
 
-import { Card, Title, Text, Container, Flex, Divider } from "@mantine/core"
-import Link from "next/link"
-import Markdown from "markdown-to-jsx"
-
+import PostCard from "./PostCard"
 import { api } from "~/trpc/react"
 
 export default function PostList() {
@@ -11,26 +8,12 @@ export default function PostList() {
 
   return (
     <>
+      <p>Post List</p>
       {postList?.map((post) => (
-        <Card shadow="sm" padding="lg" radius="md" withBorder>
-          <Title order={1} lineClamp={2}>{post.title}</Title>
-          {post.slug ? <Link href={window.location.host + "/" + post.slug}><Text c="blue">{window.location.host + "/" + post.slug}</Text></Link> : null}
-          <Flex wrap="wrap" my="xs">
-          {post.tags ? post.tags.split(",").map((tag, index) => (
-              <Container bg="#f1f3f5" size="sm" px={10} mx={5} style={{ borderRadius: '10px', padding: 0, margin: 'auto', display: 'inline-block' }}>
-                <Text key={index}>#{tag}</Text>
-              </Container>
-            )) : null}
-          </Flex>
-
-          <Divider my="md" />
-
-          <Markdown>{post.body}</Markdown>
-        </Card>
-      )
+          <PostCard post={post}/>
+        )
       )}
 
-      <p>Post List</p>
     </>
   )
 }
