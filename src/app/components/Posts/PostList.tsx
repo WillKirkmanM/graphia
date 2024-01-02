9804960c89ccc387c2a62d2f933e486b9b8c2a74
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-01-02 14:04:02 +0000

    Feat(PostList): Added a Skeleton Loading Animation

--- src/app/components/Posts/PostList.tsx
+++ src/app/components/Posts/PostList.tsx
@@ -1,19 +1,62 @@
 "use client"
 
-import PostCard from "./PostCard"
-import { api } from "~/trpc/react"
+import { useState, useEffect } from "react";
+import PostCard from "./PostCard";
+import { Skeleton } from "@mantine/core";
+import { api } from "~/trpc/react";
 
 export default function PostList() {
-  const postList = api.post.getAll.useQuery().data
+  const [loading, setLoading] = useState(true);
+  const postListQuery = api.post.getAll.useQuery();
+
+  useEffect(() => {
+    if (postListQuery.data) {
+      setLoading(false);
+    }
+  }, [postListQuery.data]);
 
   return (
     <>
-      <p>Post List</p>
-      {postList?.map((post) => (
-          <PostCard post={post}/>
-        )
-      )}
+      {loading ? (
+        <>
+        <Skeleton height={300} p="md" my="lg" />
+        <Skeleton height={30} p="md" my="lg" radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        <Skeleton height={300} p="md" my="lg" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} width="50%"radius="xl" />
+        <Skeleton height={8} mt={6} width="40%" radius="xl" />
 
+        <Skeleton height={300} p="md" my="lg" />
+        <Skeleton height={30} p="md" my="lg" radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        <Skeleton height={8} mt={6} width="70%" radius="xl" />
+        <Skeleton height={300} p="md" my="lg" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} radius="xl" />
+        <Skeleton height={8} mt={6} width="50%"radius="xl" />
+        <Skeleton height={8} mt={6} width="40%" radius="xl" />
+
+        </>
+      ) : (
+        <>
+          {postListQuery.data?.map((post) => (
+            <PostCard post={post} />
+          ))}
+        </>
+      )}
     </>
-  )
+  );
 }
