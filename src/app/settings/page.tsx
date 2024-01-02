713f4adbf933a2090996564407fb30898a08f398
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-01-02 14:39:17 +0000

    Feat(settings): Added Changing Username Notification

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -5,17 +5,29 @@ import { IconAt } from "@tabler/icons-react"
 import { useState } from "react"
 import { api } from "~/trpc/react"
 import { useSession } from "next-auth/react"
+import { notifications } from '@mantine/notifications';
 
 export default function Settings() {
   const user = useSession();
   if (!user.data) return
   const [username, setUsername] = useState(user.data.user.username)
 
-  const userChange = api.user.updateUsername.useMutation()
+  const userChange = api.user.updateUsername.useMutation({
+    onSuccess: () => {
+        notifications.show({ title: "Username updated successfully", message: `New username: ${username}. The page will now refresh`, color: "green" });
+        setTimeout(() => {
+          window.location.reload()
+        }, 5000)
+    },
+    onError: (error) => {
+      notifications.show({ title: "Error updating username", message: error.message, color: "red" });
+    }
+  });
+
   return (
     <>
-      <TextInput leftSection={<IconAt />} placeholder="Username" value={username} onChange={(e) => setUsername(e.currentTarget.value)}/>
-      <Button onClick={() => userChange.mutate({ id: user.data?.user.id, username })}>Change Username</Button>
+      <TextInput leftSection={<IconAt />} placeholder="Username" value={username} onChange={(e) => setUsername(e.currentTarget.value) }/>
+      <Button onClick={() => userChange.mutate({ id: user.data?.user.id, username }) }>Change Username</Button>
     </>
   )
 }
