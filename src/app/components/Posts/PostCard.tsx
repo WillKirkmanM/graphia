c6f88f331ac1d6eca5a57db66a3545c3139fafe6
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-01-04 20:15:20 +0000

    PKG(package): Added Text Editor

--- src/app/components/Posts/PostCard.tsx
+++ src/app/components/Posts/PostCard.tsx
@@ -23,7 +23,9 @@ export default function PostCard({ post }: PostCardProps) {
 
     <Divider my="md" />
 
-    <Markdown>{post.body}</Markdown>
+    <div className="markdown-content">
+      <Markdown>{post.body}</Markdown>
+    </div>
   </Card>
   )
 }
