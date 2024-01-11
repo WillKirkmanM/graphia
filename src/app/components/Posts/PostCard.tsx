f5cfb49a7efe507eed39f909daea1450bf86405d
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-01-11 22:17:16 +0000

    Feat(PostCard): Added Estimated Reading Time & Date Formatting

--- src/app/components/Posts/PostCard.tsx
+++ src/app/components/Posts/PostCard.tsx
@@ -1,5 +1,5 @@
-import { Card, Title, Text, Flex, Divider, Container, Anchor } from "@mantine/core"
-import Editor from "../Editor/Editor"
+import { Card, Title, Text, Flex, Divider, Container, Anchor, Group } from "@mantine/core"
+import { readingTime } from 'reading-time-estimator'
 import type { Post } from "@prisma/client"
 
 interface PostCardProps {
@@ -8,15 +8,44 @@ interface PostCardProps {
 
 export default function PostCard({ post }: PostCardProps) {
   const url = `${window.location.host}/post/${post.slug}`
+  const dateDiffDays = Math.floor((new Date().getTime() - new Date(post.createdAt).getTime()) / (1000 * 60 * 60 * 24));
+  const metric = dateDiffDays > 7 ? (dateDiffDays > 30 ? (dateDiffDays > 365 ? 'year' : 'month') : 'week') : 'day'
+  
+  let dateDiffMetric;
+  switch(metric) {
+    case 'day':
+      dateDiffMetric = dateDiffDays;
+      break;
+    case 'week':
+      dateDiffMetric = Math.floor(dateDiffDays / 7);
+      break;
+    case 'month':
+      dateDiffMetric = Math.floor(dateDiffDays / 30);
+      break;
+    case 'year':
+      dateDiffMetric = Math.floor(dateDiffDays / 365);
+      break;
+  }
+
+  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+  const timeAgo = rtf.format(-dateDiffMetric, metric);
 
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
     <Title order={1} lineClamp={2}>{post.title}</Title>
+
+    <Group gap="xs">
+      <Text>{readingTime(post.body).text}</Text>
+      <Text>·</Text>
+      <Text>{readingTime(post.body).words} words</Text>
+      <Text>·</Text>
+      <Text>{timeAgo}</Text>
+    </Group>
     <Anchor href={window.location.protocol + "//" + url} target="_blank">{url}</Anchor> 
     <Flex wrap="wrap" my="xs">
     {post.tags ? post.tags.split(",").map((tag) => (
         <Container bg="#f1f3f5" size="sm" px={10} mx={5} style={{ borderRadius: '10px', padding: 0, margin: 'auto', display: 'inline-block' }}>
-          <Text>#{tag}</Text>
+          <Text>{tag}</Text>
         </Container>
       )) : null}
     </Flex>
