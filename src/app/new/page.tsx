6eb7feb13b308aad22f5cf64525288e124b3e317
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-01-11 22:18:27 +0000

    Feat(new): Added a ShowToolbar Button

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Textarea, Fieldset, Button, ActionIcon, Group } from "@mantine/core";
+import { Textarea, Fieldset, Button, ActionIcon, Group, Switch } from "@mantine/core";
 import Editor from "../components/Editor/Editor";
 import { IconSettings } from "@tabler/icons-react";
 import { useState } from "react";
@@ -11,10 +11,11 @@ import PostCard from "../components/Posts/PostCard";
 
 export default function New() {
   const [title, setTitle] = useState("")
-  const [tags, setTags] = useState("")
+  const [tags, setTags] = useState([""])
   const [body, setBody] = useState("")
   const [slug, setSlug] = useState("")
   const [preview, setPreview] = useState(false)
+  const [showToolbar, setShowToolbar] = useState(true)
 
   const createPost = api.post.create.useMutation()
   return (
@@ -25,19 +26,18 @@ export default function New() {
             <Textarea placeholder="Your Post Title" value={title} autosize minRows={1} mb={10} onChange={(e) => { setTitle(e.currentTarget.value); setSlug(slugify(e.currentTarget.value)) }}/>
             <Textarea placeholder="The desired url" value={slug} autosize minRows={1} mb={10} onChange={(e) => setSlug(e.currentTarget.value)}/>
             {/* <MultiSelect placeholder="Enter up to 5 tags..." data={tagList ? [...tagList].sort((a, b) => b.popularity - a.popularity).map(tag => tag.name) : []} onChange={(e) => setTags(e.join(","))} value={tags ? tags.split(",") : []} maxValues={5} searchable /> */}
-            <TagSelection />
+            <TagSelection tags={tags} setTags={setTags}/>
             {/* <Textarea placeholder="Write your post content here..." value={body} autosize minRows={4} mt={10} onChange={(e) => setBody(e.currentTarget.value)}/> */}
-            <Editor body={body} setBody={setBody}/>
+            <Editor body={body} setBody={setBody} showToolbar={showToolbar}/>
           </>
         ) : (
           <>
-
-            <PostCard post={{ title, slug, tags, body, createdAt: new Date(), updatedAt: new Date(), createdById: "0", id: 0}}/>
+            <PostCard post={{ title, slug, tags: tags.join(", "), body, createdAt: new Date(), updatedAt: new Date(), createdById: "0", id: 0}}/>
           </>
         )}
 
         <Group py="md">
-          <Button onClick={() => createPost.mutate({ title, slug, tags, body }) }>Publish</Button>
+          <Button onClick={() => createPost.mutate({ title, slug, tags: tags.join(", "), body }) }>Publish</Button>
           <Button variant="outline">Save Draft</Button>
 
           <ActionIcon variant="default" color="gray" aria-label="Settings" size="lg">
@@ -48,6 +48,11 @@ export default function New() {
             {preview ? "Edit" : "Preview"}
           </Button>
 
+          <Switch
+            label="Show Toolbar"
+            defaultChecked
+            onChange={(e) => setShowToolbar(e.currentTarget.checked)}
+          />
         </Group>
       </Fieldset>
     </>
