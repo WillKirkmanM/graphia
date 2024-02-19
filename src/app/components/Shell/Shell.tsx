3ba1f5d02a716e719f89a2d27ff9156dee8ae347
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-02-19 20:59:08 +0000

    Feat(Shell): Removed Unused Imports & Removed Navbar

--- src/app/components/Shell/Shell.tsx
+++ src/app/components/Shell/Shell.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-import { ActionIcon, AppShell, Burger, Button, Group, TextInput, Text, Space, NavLink, Flex, Grid, Popover } from '@mantine/core';
+import { ActionIcon, AppShell, Button, Group, Text } from '@mantine/core';
 import AvatarDropdown from '../User/AvatarDropdown';
 import Link from 'next/link';
 
-import { IconBell, IconBrandGoogle, IconHome2, IconLogin, IconPlus, IconSearch, IconWriting } from "@tabler/icons-react"
-import { useDisclosure } from '@mantine/hooks';
+import { IconBell, IconPlus, IconSearch } from "@tabler/icons-react"
 import { signIn, useSession } from 'next-auth/react';
-import { useState } from 'react';
 import Search from '../Search/Search';
 import { GoogleIcon } from '../Icons/GoogleIcon';
 
@@ -16,32 +14,17 @@ interface ShellProps {
 }
 
 export default function Shell({ children }: ShellProps) {
-  const [opened, { toggle }] = useDisclosure();
-  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
-  const [showMobileSignIn, setShowMobileSignIn] = useState(false);
   const session = useSession();
 
   return (
     <>
       <AppShell
         header={{ height: { base: 50, md: 60, lg: 70 } }}
-        navbar={{
-          width: { base: 200, md: 300, lg: 300 },
-          breakpoint: 'sm',
-          collapsed: { desktop: !desktopOpened, mobile: !opened },
-        }}
         padding="md"
       >
         <AppShell.Header>
           <Group h="100%" justify="space-between" mx="sm">
           <Group justify="flex-start">
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="sm" 
-            />
-            <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
             <Link href="/">
               <Text>Graphia</Text>
             </Link>
@@ -76,17 +59,7 @@ export default function Shell({ children }: ShellProps) {
           </Group>
           </AppShell.Header>
 
-        <AppShell.Navbar p="md">
-        <NavLink
-          href="/"
-          label="Home"
-          leftSection={<IconHome2 size="1rem" stroke={1.5} />} />
-        <NavLink
-          href="/new"
-          label="Write Post"
-          leftSection={<IconWriting size="1rem" stroke={1.5} />} />
-        </AppShell.Navbar>
-      <AppShell.Main>{children}</AppShell.Main>
+          <AppShell.Main>{children}</AppShell.Main>
     </AppShell>
       </>
   );
